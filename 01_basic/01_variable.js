d53172505e0bc05b09215c8f8d23f83e6a90414b
Adityakhare123 <102724471+Adityakhare123@users.noreply.github.com>
2024-07-03 06:42:01 +0000

    datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+// accountId = 1
+
+const accountId=14456
+let accountEmail="khareaditya0812@gmail.com"
+var accountPassword="1234"
+accountCity="Indore"
+
+// accountId = 2
+accountEmail="aaditya0812@gmail.com"
+accountPassword="1235"
+accountCity="Pune"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,23 @@
+console.log(6+6)
+console.log("Aditya")
+// alert("Somethings went wrong")
+
+console.log(typeof "Aditya")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+const score=100
+const scorevalue=100.5
+const isloggedIn=false
+const outsideTemp=null
+let useremail;
+const id=symbol('123')
+const anotherId = symbol('123')
+console.log(id === anotherId);
+
+const hero =["Ironman","Spiderman","Hulk","Thor"];
+// const function yogi(params) {
+//     console.log("hello world")
+// }
+// console)
