59c811b03ed830bb513e072656eab8c406452bee
Adityakhare123 <102724471+Adityakhare123@users.noreply.github.com>
2024-07-03 07:43:01 +0000

    ConversionOperations

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,22 @@
+// let score = "10";
+// // let score = null;
+// // let score = true;
+// // let score = false;
+// // let score = undefine;
+// // let score = "10abc"; NaN value
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueofNumber = Number(score)
+// console.log(typeof valueofNumber);
+// console.log(valueofNumber);
+
+
+// let isloggedIn =1;
+// let booleanisloggedIn=Boolean(isloggedIn)
+// console.log(booleanisloggedIn)
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
