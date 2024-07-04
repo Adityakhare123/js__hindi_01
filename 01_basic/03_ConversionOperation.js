2394709fccd0a962497e85ad5c8effedfff02ed3
Adityakhare123 <102724471+Adityakhare123@users.noreply.github.com>
2024-07-04 08:11:10 +0000

    com

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -16,7 +16,58 @@
 // let booleanisloggedIn=Boolean(isloggedIn)
 // console.log(booleanisloggedIn)
 
-let somenumber = 33
-let stringnumber = String(somenumber)
-console.log(stringnumber)
-console.log(typeof stringnumber)
+// let somenumber = 33
+// let stringnumber = String(somenumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+
+// *******************Operations *******************
+
+// let value =3
+// let negativeValue = -value
+// console.log(negativeValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "Hello"
+// let str2  = " Aditya"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// let gamecounter = 100;
+// gamecounter++
+// console.log(gamecounter)
+
+// *******************************************
+
+// let Youtube = "AdityaKharecom"
+// let anotherName = "myyoutube"
+// anotherName="Ronakcom"
+
+// console.log(Youtube)
+// console.log(anotherName)
+
+// *******************************************
+
+let userOne = {
+    email:"Aditya@gmail.com",
+    upi:"Aditya@ypl"
+}
+
+let userTwo = userOne
+
+userTwo.email="Ronak@googlecom"
+
+console.log(userOne.email)
+console.log(userTwo.email)
